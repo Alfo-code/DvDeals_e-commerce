ef3efa8d846c532359c9ff4073914e0a4245ba44
Alfo-code <alfostarace@gmail.com>
2022-11-14 11:55:47 +0000

    search added

--- data.js
+++ data.js
@@ -1,17 +1,18 @@
 let data = [
     {
-        "id": 158,
-        "title": "It",
+        "id": 1,
+        "title": "Deadpool",
         "genre": [
-          "Horror"
+          "Action",
+          "Comedy"
         ],
-        "year": 1989,
-        "price": "£12.09",
+        "year": 2016,
+        "price": "£15.50",
         "bluray": true,
-        "stock": 6
+        "stock": 2
       },
       {
-        "id": 159,
+        "id": 2,
         "title": "Paranormal Activity",
         "genre": [
           "Horror",
@@ -23,20 +24,19 @@ let data = [
         "stock": 5
       },
       {
-        "id": 160,
-        "title": "Thieves",
+        "id": 3,
+        "title": "Donnie Darko",
         "genre": [
-          "Crime",
           "Drama",
-          "Romance"
+          "Mystery"
         ],
-        "year": 1995,
-        "price": "£2.22",
+        "year": 2001,
+        "price": "£6.99",
         "bluray": false,
-        "stock": 3
+        "stock": 1
     },
     {
-        "id": 161,
+        "id": 4,
         "title": "Eat",
         "genre": [
           "Drama"
@@ -47,7 +47,7 @@ let data = [
         "stock": 11
     },
     {
-        "id": 162,
+        "id": 5,
         "title": "Paranormal Activity 2",
         "genre": [
           "Horror",
@@ -59,7 +59,7 @@ let data = [
         "stock": 11
     },
     {
-        "id": 163,
+        "id": 6,
         "title": "Thieves (Voleurs, Les)",
         "genre": [
           "Crime",
@@ -72,39 +72,150 @@ let data = [
         "stock": 13
     },
     {
-        "id": 164,
-        "title": "Eat",
+        "id": 7,
+        "title": "Inception",
         "genre": [
-          "Drama"
+          "Sci-Fi"
         ],
-        "year": 2000,
-        "price": "£12.09",
+        "year": 2010,
+        "price": "£15.99",
         "bluray": true,
-        "stock": 11
+        "stock": 4
     },
     {
-        "id": 165,
-        "title": "Paranormal Activity 2",
+        "id": 8,
+        "title": "The Matrix",
         "genre": [
-          "Horror",
-          "IMAX"
+          "Action",
+          "Sci-Fi"
         ],
-        "year": 1986,
-        "price": "£9.65",
+        "year": 1999,
+        "price": "£9.50",
         "bluray": false,
-        "stock": 11
+        "stock": 9
     },
     {
-        "id": 166,
-        "title": "Thieves (Voleurs, Les)",
+        "id": 9,
+        "title": "Interstellar",
         "genre": [
-          "Crime",
+          "Adventure",
           "Drama",
-          "Romance"
+          "Sci-Fi"
         ],
-        "year": 1995,
-        "price": "£2.22",
-        "bluray": false,
-        "stock": 13
-    }
+        "year": 2014,
+        "price": "£22.00",
+        "bluray": true,
+        "stock": 18
+    },
+    {
+      "id": 10,
+      "title": "Alien",
+      "genre": [
+        "Horror"
+      ],
+      "year": 1979,
+      "price": "£8.99",
+      "bluray": false,
+      "stock": 6
+    },
+    {
+      "id": 11,
+      "title": "Back to the Future",
+      "genre": [
+        "Adventure",
+        "Sci-Fi"
+      ],
+      "year": 1985,
+      "price": "£9.99",
+      "bluray": false,
+      "stock": 15
+    },
+    {
+      "id": 12,
+      "title": "The Prestige",
+      "genre": [
+        "Drama",
+        "Mystery",
+        "Sci-Fi"
+      ],
+      "year": 2006,
+      "price": "£13.90",
+      "bluray": true,
+      "stock": 13
+  },
+  {
+      "id": 13,
+      "title": "Avengers: Endgame",
+      "genre": [
+        "Action",
+        "Drama"
+      ],
+      "year": 2019,
+      "price": "£20.09",
+      "bluray": true,
+      "stock": 1
+  },
+  {
+      "id": 14,
+      "title": "Avengers: Infinity War",
+      "genre": [
+        "Action",
+        "Sci-Fi"
+      ],
+      "year": 2018,
+      "price": "£19.99",
+      "bluray": true,
+      "stock": 0
+  },
+  {
+      "id": 15,
+      "title": "V for Vendetta",
+      "genre": [
+        "Action",
+        "Drama",
+        "Sci-Fi"
+      ],
+      "year": 2005,
+      "price": "£21.02",
+      "bluray": false,
+      "stock": 5
+  },
+  {
+      "id": 16,
+      "title": "Jurassic Park",
+      "genre": [
+        "Action",
+        "Adventure",
+        "Sci-Fi"
+      ],
+      "year": 1993,
+      "price": "£10.00",
+      "bluray": false,
+      "stock": 4
+  },
+  {
+      "id": 17,
+      "title": "The Thing",
+      "genre": [
+        "Horror",
+        "Mystery"
+      ],
+      "year": 1982,
+      "price": "£8.65",
+      "bluray": false,
+      "stock": 2
+  },
+  {
+      "id": 18,
+      "title": "Logan",
+      "genre": [
+        "Action",
+        "Drama",
+        "Sci-Fi"
+      ],
+      "year": 2017,
+      "price": "£13.20",
+      "bluray": true,
+      "stock": 9
+  }
 ];
--- index.js
+++ index.js
@@ -1,15 +1,181 @@
-
+// Cards
 let dvd = "";
 data.forEach((movies) =>{
     dvd += 
-    `<div class="cards">
+    `<div class="cards" id="cards">
         <h2 class="title">${movies.title}</h2>
-        <img src="" alt="" class="poster">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/DVD_logo.svg/768px-DVD_logo.svg.png?20220618105447" alt="dvd" class="poster">
         <p>${movies.genre}</p>
-        <p>Years: ${movies.year}</p>
-        <p>${movies.price}</p>
-        <p>Stock: ${movies.stock}</p>
-        <button class="btn" type="submit">BUY NOW</button>
+        <p>Year: ${movies.year}</p>
+        <p id="prices">${movies.price}</p>
+        <p>In Stock: ${movies.stock}</p>
+        <button class="btn" id="btn" type="submit">Add to Basket</button>
     </div>`
 })
 document.getElementById('movie-grid').innerHTML = dvd;
+
+// =========================================================
+
+// Sort by date oldestFirst
+
+let oldest = data.slice(0);
+
+oldest.sort(function(a,b) {
+    return a.year - b.year;
+});
+
+
+
+let sorted = "";
+oldest.forEach((movies) =>{
+    sorted += 
+    `<div class="cards" id="cards">
+        <h2 class="title">${movies.title}</h2>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/DVD_logo.svg/768px-DVD_logo.svg.png?20220618105447" alt="dvd" class="poster">
+        <p>${movies.genre}</p>
+        <p>Year: ${movies.year}</p>
+        <p>${movies.price}</p>
+        <p>In Stock: ${movies.stock}</p>
+        <button class="btn" id="btn" type="submit">Add to Basket</button>
+    </div>`
+})
+
+
+
+const old = document.getElementById('dropdown');
+
+old.addEventListener('click', ()=>{
+  document.getElementById('movie-grid').innerHTML = sorted;
+})
+
+// Sort by date newestFirst
+
+let newest = data.slice(0);
+
+newest.sort(function(a,b) {
+    return b.year - a.year;
+});
+
+let sortedNew = "";
+oldest.forEach((movies) =>{
+    sortedNew += 
+    `<div class="cards" id="cards">
+        <h2 class="title">${movies.title}</h2>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/DVD_logo.svg/768px-DVD_logo.svg.png?20220618105447" alt="dvd" class="poster">
+        <p>${movies.genre}</p>
+        <p>Year: ${movies.year}</p>
+        <p>${movies.price}</p>
+        <p>In Stock: ${movies.stock}</p>
+        <button class="btn" type="submit">Add to Basket</button>
+    </div>`
+})
+
+
+
+// const dvdNew = document.getElementById('cards');
+
+// dvdNew.addEventListener('click', ()=>{
+//   document.getElementById('movie-grid').innerHTML = sortedNew;
+// })
+
+// Search
+
+function searchDvd(){
+  const input = document.getElementById('search').value.toUpperCase();
+  const cardContainer = document.getElementById('movie-grid');
+
+  const movie = cardContainer.getElementsByClassName('cards');
+
+  for(let i = 0; i < movie.length; i++) {
+    let title = movie[i].querySelector('.title');
+
+    if(title.innerText.toUpperCase().indexOf(input) > -1) {
+      movie[i].style.display = "";
+    } else {
+      movie[i].style.display = "none";
+    }
+  }
+};
+
+
+
+// Add to Cart
+
+const addItem = document.getElementById('btn');
+
+addItem.addEventListener('click', ()=>{
+  console.log(addItem);
+  document.getElementById('cart-counter').innerText = +1;
+})
+
+// ///
+
+const inStock = data.slice(0);
+
+inStock.sort(function(a,b) {
+  return a.stock - b.stock;
+});
+console.log(inStock);
+
+function addToCart(){
+  const btn = document.getElementById('btn');
+  const cartCounter = document.getElementById('cart-counter');
+ 
+
+  // for(let i = 0; i < movie.length; i++) {
+  //   let title = movie[i].querySelector('.title');
+
+  //   if(title.innerText.toUpperCase().indexOf(input) > -1) {
+  //     movie[i].style.display = "";
+  //   } else {
+  //     movie[i].style.display = "none";
+  //   }
+  // }
+};
+
+
+// (function(){
+
+//     const cart = document.getElementById('cart');
+//     const cartContainer = document.getElementById('cart');
+  
+//     cart.addEventListener('click', function(){
+//       cartContainer.classList.toggle('show-cart')
+//     })
+//   }
+// )();
+
+
+// Filter
+function genre() {
+    data.map((item, i) =>{
+    return item.filter(movie => movie.genre == "Horror");
+    
+})
+}
+console.log();
+
+// aaaaaaaaaaaa
+
+// function sortCards(property, cards) {
+//     cards.sort(function(a,b){
+//       //Return 0 if the same
+//       if(a[property] == b[property] ) {
+//         return 0;
+//       }
+      
+//       //Grab property values
+//       var a = a[property]; 
+//       var b = b[property];
+      
+//       //Handle numbers correctly
+//       if(property == "price") {
+//         //Make numbers if price - strip $ and ,
+//         a = parseFloat(a.replace(/\£|,/g, ""));
+//         b = parseFloat(b.replace(/\£|,/g, ""));
+//       }
+      
+//       //Normal sort return
+//       return a > b ? 1 : -1;
+//     })
+//   }
